74bd7b96e8a5f7ec731ea06c2844100094dc1379
simochee <lollipop@simochee.net>
2025-02-15 23:47:33 +0900

    feat: gyazo worker

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/gyazo/:id', async (c) => {
+  const id = c.req.param('id')
+  
+  return await fetch(`https://i.gyazo.com/${id}.png`, {
+    cf: {
+      cacheEverything: true,
+      cacheTtl: 60 * 60 * 24 * 30,
+    }
+  })
+})
+
+export default app 
